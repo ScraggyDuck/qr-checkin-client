31bf85a0e7c45b493742c6c2641625258ed40423
JokerV <Quocvietcoi2000@gmail.com>
2020-03-15 15:14:21 +0700

    setup

---
+++ src/pages/register.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import QRCode from 'qrcode.react';
+
+// const downloadQR = () => {
+//     const canvas = document.getElementById('qrcode');
+//     const pngUrl = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+//     console.log('pngUrl', pngUrl);
+//     let downloadLink = document.createElement('a');
+//     downloadLink.href = pngUrl;
+//     downloadLink.download = 'qr.png';
+//     document.body.appendChild(downloadLink);
+//     downloadLink.click();
+//     document.body.removeChild(downloadLink);
+// };
+
+function Register () {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Register;
