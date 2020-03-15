003ecfb03b39e7e4acb5b5541fd6c01f6e04d078
JokerV <Quocvietcoi2000@gmail.com>
2020-03-16 01:19:31 +0700

    fix scanner

--- src/components/login.js
+++ src/components/login.js
@@ -10,9 +10,19 @@ import attendanceServices from '../services/attendance.service';
 import { NavLink } from 'react-router-dom';
 
 export default class ScannerQR extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShowScanner: true
+    };
+  }
+
   handleScan = async userId => {
     if (userId) {
+      this.setState({ isShowScanner: false });
       let user = await userServices.checkUser(userId);
+
       if (user) {
         const isCheckIn = await attendanceServices.isCheckIn(userId);
         if (isCheckIn) {
@@ -22,10 +32,7 @@ export default class ScannerQR extends Component {
               userId: user._id,
               isCheckIn: false
             })
-            .then(res => {
-              console.log(res);
-              toastSuccess('Đăng xuất thành công!');
-            })
+            .then(res => toastSuccess('Đăng xuất thành công!'))
             .catch(err => toastError(err));
         } else {
           attendanceServices
@@ -37,7 +44,10 @@ export default class ScannerQR extends Component {
             .then(res => toastSuccess('Đăng nhập thành công!'))
             .catch(err => toastError(err));
         }
+      } else {
+        toastError('Mã QR không tồn tại!');
       }
+      this.setState({ isShowScanner: true });
     }
   };
 
@@ -60,12 +70,21 @@ export default class ScannerQR extends Component {
             </NavLink>
             <h3 className='title'>Check In bằng QR Code</h3>
             <div className='mx-3'>
-              <QrReader
-                delay={1000}
-                onError={this.handleError}
-                onScan={this.handleScan}
-                style={{ width: '100%' }}
-              />
+              {this.state.isShowScanner ? (
+                <QrReader
+                  delay={300}
+                  onError={this.handleError}
+                  onScan={this.handleScan}
+                  style={{ width: '100%' }}
+                />
+              ) : (
+                <div>
+                  <div className='spinner-border mt-2' role='status'>
+                    <span className='sr-only'></span>
+                  </div>
+                  <h4>Đang xử lí!</h4>
+                </div>
+              )}
             </div>
           </div>
         </div>
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://localhost:4000/api/v1'
+export const API_BASE_URL = 'https://qr-checkin-server.herokuapp.com/api/v1/'
